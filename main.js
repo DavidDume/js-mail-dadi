edf707298e4741c672adecd47a9f022e473d7b99
David Dume <dumedavid11@gmail.com>
2023-02-27 15:44:07 +0100

    codice per stabilire chi vince

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const userEmail = prompt("Inserisci la tua email per giocare:");
+const userEmail = "mail1@mail.com";
 const emails = ["mail1@mail.com", "mail2@mail.com", "mail3@mail.com", "mail@email.it", "mail8@mail.it"];
 
 // controllo ogni singola email in "emails"
@@ -10,5 +10,12 @@ for(let i = 0; i < emails.length; i++) {
         let dadiPc = Math.floor(Math.random() * (7 - 1) + 1); // genera numero random tra 1 e 6
         let dadiUser = Math.floor(Math.random() * (7 - 1) + 1 );
         console.log(dadiPc, dadiUser);
+        if(dadiPc > dadiUser) {
+            console.log("PC vince!");
+        } else if(dadiUser > dadiPc) {
+            console.log("TU Vinci");
+        } else {
+            console.log("pareggio");
+        }
     }
 }
